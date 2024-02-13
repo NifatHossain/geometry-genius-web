40dcafd08effd31e97fa186d19055dfa6194f236
NifatHossain <nifathossain1480@gmail.com>
2024-02-13 23:24:16 +0600

    Function's arguments not working

--- scripts/triangle.js
+++ scripts/triangle.js
@@ -2,9 +2,21 @@ function getInput(id){
     const num= parseFloat(document.getElementById(id).value);
     return num;
 }
-function getArea(){
-    const base= getInput('triangleBase');
-    const height= getInput('triangleHeight');
+function getArea(objName, value1, value2){
+    const a= `\'${objName}`+`${value1}\'`;
+    console.log(typeof a);
+    const base= getInput(a);
+    // const base= getInput('triangleBase');
+    // const base= getInput(`\'${objName}`+`${value1}\'`);
+    const b= `\'${objName}`+`${value2}\'`;
+    const height= getInput(b);
+    // const height= getInput('triangleHeight');
+    // const height= getInput(`\'${objName}`+`${value2}\'`);
     const area= .5*base*height;
-    console.log(area);
+    const div= document.getElementById('answers') ;
+    const text= document.createElement('p')
+    text.innerText= `Area of ${objName} : ${area}`
+    div.appendChild(text);
+    //  console.log(`Area of triangle : ${area}`)
+    console.log(`\'${objName}`+`${value1}\'`)
 }
